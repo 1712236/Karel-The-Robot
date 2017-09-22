16387e1d49227e8aaf8140ebca26667943936439
S <s>
2017-09-22 21:58:13 +0600

    {"types":"C","runs":44}

--- karelNew.java
+++ karelNew.java
@@ -1,4 +1,20 @@
+import stanford.karel.SuperKarel;
 
-public class karelNew {
-
+public class karelNew extends karel {
+	public void run() {
+    for (int k = 0; k < 5; k++) {
+		turnLeft();
+		for (int r = 0; r < 9; r++) {
+			move();
+		}
+		turnRight();
+		move();
+		turnRight();
+		for (int r = 0; r < 9; r++ ) {
+			move();
+		}
+		turnLeft();
+		move();
+    }		
+ }
 }
