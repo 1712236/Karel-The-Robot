463ce7395354db4b48ea6550f944bb30c9da55a1
S <s>
2017-09-24 22:57:58 +0600

    {"types":"C","runs":175}

--- try1.java
+++ try1.java
@@ -2,6 +2,11 @@ import stanford.karel.SuperKarel;
 
 public class try1 extends SuperKarel {
 	public void run() {
-		
+		for (int k = 0; k < 9; k++) {
+			move();
+			if (beepersPresent()) {
+				pickBeeper();
+			}
+		}
 	}
 }
