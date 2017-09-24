f377ef200b2f1e1808d3fa0129ac5f30defde78a
S <s>
2017-09-24 19:08:55 +0600

    {"types":"C","runs":124}

--- beeper.java
+++ beeper.java
@@ -4,6 +4,10 @@ public class beeper extends SuperKarel {
 	public void run() {
 		while (frontIsClear()) {
 			move();
+			while (frontIsBlocked()) {
+				turnLeft();
+				move();
+			}
 		}
 			}
 }
