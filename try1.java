daf11fedb7899e66de1829564f072817c61eefb1
S <s>
2017-09-24 23:41:27 +0600

    {"types":"C","runs":193}

--- try1.java
+++ try1.java
@@ -2,40 +2,6 @@ import stanford.karel.SuperKarel;
 
 public class try1 extends SuperKarel {
 	public void run() {
-		for (int k = 0; k < 9; k++) {
-			move();
-			if (beepersPresent()) {
-				pickBeeper();
-			}
-		}
-		turnLeft();
-		move();
-		turnLeft();
-		pickBeeper();
-		for (int k = 0; k < 9; k++) {
-			move();
-			if (beepersPresent()) {
-				pickBeeper();
-			}
-		}
-		turnRight();
-		move();
-		turnRight();
-		for (int k = 0; k < 9; k++) {
-			move();
-			if (beepersPresent()) {
-				pickBeeper();
-			}
-		}
-		turnLeft();
-		move();
-		turnLeft();
-		for (int k = 0; k < 9; k++) {
-			move();
-			if (beepersPresent()) {
-				pickBeeper();
-			}
-		}
 		west();
 		east();
 		west();
