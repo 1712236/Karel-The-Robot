26eaae51bc0e4b845de1d77c387c7de7d1e745f9
S <s>
2017-09-24 19:40:51 +0600

    {"types":"C","runs":152}

--- beeper.java
+++ beeper.java
@@ -2,7 +2,7 @@ import stanford.karel.*;
 
 public class beeper extends SuperKarel {
 	public void run() {
-		for (int i = 0; i < 1; i++) {
+		for (int i = 0; i < 0; i++) {
 			while (frontIsClear()) {
 				move();
 				if (beepersPresent()) {
