8bda5f02b334e241f59851b31ed544bdd060ae8b
S <s>
2017-09-24 20:56:08 +0600

    {"types":"C","runs":159}

--- putbeeper.java
+++ putbeeper.java
@@ -2,6 +2,18 @@ import stanford.karel.SuperKarel;
 
 public class putbeeper extends SuperKarel {
 	public void run() {
-		
+		while (frontIsClear()) {
+			move();
+			if (beepersPresent()) {
+				pickBeeper();
+			}
+			else {
+				putBeeper();
+			}
+			if (frontIsBlocked()) {
+				turnLeft();
+				move();
+			}
+		}
 	}
 }
